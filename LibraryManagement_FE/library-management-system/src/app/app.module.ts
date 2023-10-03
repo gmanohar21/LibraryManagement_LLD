afce5af703d316ea921301a265db64c297d6a718
subhashismollick <smollickcseiu@gmail.com>
2023-10-04 01:45:41 +0600

    add

--- LibraryManagement_FE/library-management-system/src/app/app.module.ts
+++ LibraryManagement_FE/library-management-system/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+
 @NgModule({
   declarations: [
     AppComponent
@@ -13,7 +14,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
